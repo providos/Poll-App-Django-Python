297586a3fbd081caa0d5ff41d4b4df574730eeac
Oliver <39782234+providos@users.noreply.github.com>
2018-10-02 20:29:31 +0300

    First update
    Creating the first view of the poll app with an hello world using python's modules.

--- views.py
+++ views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse 
+# in django we use modules, these modules are built in scripts to help us automate certain tasks
+# in this case we use the "import" statement to import the submodule .http from the django library?
+# from the .http module we use theHttpResponse script
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
